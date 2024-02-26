0e74ea85219e6709de125b1ea98795507a2b3c80
GeorgeReiter1 <149304951+GeorgeReiter1@users.noreply.github.com>
2024-02-26 11:43:52 +0300

    task1.py

--- ООП_Лабораторная работа 3/task1.py
+++ ООП_Лабораторная работа 3/task1.py
@@ -1,55 +1,76 @@
-class Book:
-    """Базовый класс для книг."""
-    def __init__(self, name: str, author: str):
-        self._name = name
-        self._author = author
-
-    @property
-    def name(self):
-        return self._name
-
-    @property
-    def author(self):
-        return self._author
-
-    def __str__(self):
-        return f"Книга {self.name}. Автор {self.author}"
+class Game:
+    """Базовый класс, представляющий игру"""
+    def __init__(self, title: str, number_of_players: int, year: int):
+        """Инициализация игры"""
+        self.title = title
+        self.number_of_players = number_of_players
+        self.year = year
 
     def __repr__(self):
-        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r})"
+        """Представление объекта строковое"""
+        return f"{self.__class__.__name__}(title={self.title}, number_of_players={self.number_of_players}, year={self.year})"
+
+    def __str__(self):
+        """представление игры"""
+        return f"{self.title} ({self.year}), Количество игроков: {self.number_of_players}"
+
+    def setup(self):
+        """Подготовка игры"""
+        pass
+
+    def play(self):
+        """Начать игру"""
+        pass
 
 
-class PaperBook(Book):
-    def __init__(self, name: str, author: str, pages: int):
-        super().__init__(name, author)
-        self._pages = pages
+class VideoGame(Game):
+    """Класс, представляющий видеоигру, наследуется от Game"""
+    def __init__(self, title: str, number_of_players: int, year: int, platform: str):
+        """Инициализация видеоигры"""
+        super().__init__(title, number_of_players, year)
+        self.platform = platform
 
-    @property
-    def pages(self):
-        return self._pages
+    def install(self):
+        """Установка видеоигру"""
+        pass
 
-    @pages.setter
-    def pages(self, value):
-        if not isinstance(value, int):
-            raise ValueError("Количество страниц должно быть целым числом.")
-        if value <= 0:
-            raise ValueError("Количество страниц должно быть положительным числом.")
-        self._pages = value
+    def play_online(self):
+        """Играть в видеоигру онлайн"""
+        pass
 
 
-class AudioBook(Book):
-    def __init__(self, name: str, author: str, duration: float):
-        super().__init__(name, author)
-        self._duration = duration
+class BoardGame(Game):
+    """Класс, представляющий настольную игру, наследуется от Game"""
+    def __init__(self, title: str, number_of_players: int, year: int, material_of_manufacture: str):
+        """Инициализация настольной игры"""
+        super().__init__(title, number_of_players, year)
+        self.material_of_manufacture = material_of_manufacture
 
-    @property
-    def duration(self):
-        return self._duration
+    def setup(self):
+        """Переопределенный метод подготовки для настольной игры."""
+        # Дополнительные шаги для настройки настольной игры, например расстановка игровых элементов
+        super().setup()
 
-    @duration.setter
-    def duration(self, value):
-        if not isinstance(value, (int, float)):
-            raise ValueError("Продолжительность должна быть числом.")
-        if value <= 0:
-            raise ValueError("Продолжительность должна быть положительным числом.")
-        self._duration = value
+    def play(self):
+        """Переопределенный метод игры для учета правил конкретной настольной игры."""
+        # Правила, специфичные для настольных игр, такие как способ бросать кубики или доставать карты
+        super().play()
+
+
+if __name__ == "__main__":
+    # Создаем экземпляр настольной игры
+    monopoly = BoardGame(title="Монополия", number_of_players=2, year=1935, material_of_manufacture="Бумага и пластик")
+
+    # Создаем экземпляр видеоигры
+    minecraft = VideoGame(title="Minecraft", number_of_players=1, year=2011, platform="PC")
+
+    # Выводим информацию о настольной игре
+    print(monopoly)
+    monopoly.setup()
+    monopoly.play()
+
+    # Выводим информацию о видеоигре
+    print(minecraft)
+    minecraft.install()
+    minecraft.play_online()
+
